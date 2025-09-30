3db9813620c0c3e35e212886936b87d3c6248df5
ThiagoZanconi <thiagzanc@gmail.com>
2025-09-30 11:13:34 -0300

    Perform action update

--- routers/ai_assistant.py
+++ routers/ai_assistant.py
@@ -26,22 +26,24 @@ def chat(prompt: str, model: str = "deepseek-r1:8b"):
     return {"response": response}
 
 @router.post("/generate_image/")
-def generate_image(prompt:str):
+def generate_image(prompt:str, image_width: int = 512, image_height: int = 512, image_steps: int = 4, guidance_scale: float = 7.0):
     from main import file_path
     from ai_image_generator_module.sdxl_turbo import SDXLTurbo
     path = file_path / "generated_image.png"
-    SDXLTurbo.text_to_image(prompt, path)
+    SDXLTurbo.text_to_image(prompt, path, width=image_width, height=image_height, steps=image_steps, guidance_scale=guidance_scale)
     return {"image_path": path}
 
 @router.post("/perform_action/")
-def perform_action(path: str, user_input: str, n: int = 10, delta_threshold: float = 3.0, think: bool = False, model: str = "deepseek-r1:8b"):
+def perform_action(path: str, user_input: str, n: int = 10, delta_threshold: float = 3.0, think: bool = False, model: str = "deepseek-r1:8b", image_width: int = 512,
+                image_height: int = 512, image_steps: int = 4, guidance_scale: float = 7.0
+                   ):
     action = OllamaChatService.select_action(user_input, think = think)
     if "1" in action:
         return __change_item_color(path, user_input, n, delta_threshold, think)
     elif "2" in action:
         return __change_background_color(path)
     elif "3" in action:
-        return generate_image(user_input)
+        return generate_image(user_input, image_width=image_width, image_height=image_height, image_steps=image_steps, guidance_scale=guidance_scale)
     elif "4" in action:
         return chat(user_input, model)
     else:
